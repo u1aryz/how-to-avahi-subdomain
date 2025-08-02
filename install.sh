599d8230276fec2c195ce24af6d829218d3de167
u1aryz <u1aryz.d@gmail.com>
2025-08-03 03:12:18 +0900

    refactor: use loop to install files

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
+set -euo pipefail
+set -x
 sudo -v
-curl -fsSL https://raw.githubusercontent.com/u1aryz/how-to-avahi-subdomain/refs/heads/main/avahi-publish-subdomain |
-	sudo install -Dm755 /dev/stdin /usr/local/bin/avahi-publish-subdomain
-curl -fsSL https://raw.githubusercontent.com/u1aryz/how-to-avahi-subdomain/refs/heads/main/avahi-subdomain%40.service |
-	sudo install -Dm644 /dev/stdin /etc/systemd/system/avahi-subdomain@.service
+
+BASE_URL="https://raw.githubusercontent.com/u1aryz/how-to-avahi-subdomain/refs/heads/main"
+
+# Format: filename|permission|dist
+FILES=(
+	"avahi-publish-subdomain|755|/usr/local/bin/avahi-publish-subdomain"
+	"avahi-subdomain%40.service|644|/etc/systemd/system/avahi-subdomain@.service"
+)
+
+for entry in "${FILES[@]}"; do
+	IFS='|' read -r FILE MODE DEST <<<"$entry"
+	curl -fsSL "${BASE_URL}/${FILE}" | sudo install -Dm"${MODE}" /dev/stdin "${DEST}"
+done
